862a17f09ac15dbe18b16c14424d7f82ea0f3787
Willian <willianisami1229@gmail.com>
2024-04-23 17:51:31 -0300

    remove backend/dist folder

--- backend/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-export declare const app: import("express-serve-static-core").Express;
-export declare const server: import("http").Server<typeof import("http").IncomingMessage, typeof import("http").ServerResponse>;
-//# sourceMappingURL=index.d.ts.map
