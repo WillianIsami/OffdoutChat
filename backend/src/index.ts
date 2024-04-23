5bed0ec4412c6c6b9bcdbaef45377bf83992c2c2
Willian <willianisami1229@gmail.com>
2024-04-23 17:50:56 -0300

    feat: socket connection for sending messages, addomg users and disconnecting users

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import { Server } from 'socket.io';
 import { createServer } from 'http';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const app = express();
 app.use(cors());
@@ -10,22 +13,55 @@ export const server = createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: process.env.ORIGIN_URL,
+        origin: "*",
         methods: ["GET", "POST"],
     },
-    maxHttpBufferSize: 2e7
+    maxHttpBufferSize: 2e7,
 });
 
+app.get('/', (req, res) => {
+    res.json({"test": 1});
+});
+
+let numUsers = 0;
+
 io.on("connection", (socket) => {
+    let addedUser = false;
     console.log(`new user connected ${socket.id}`)
 
-    io.on("message", (message: string) => {
+    socket.on("message", (message: string) => {
+        socket.broadcast.emit('new message', {
+            id: socket.id,
+            message: message
+        });
         console.log("Received message: ", message);
-        io.emit("message", message);
+    });
+
+    socket.on('add user', () => {
+        if (addedUser) return;
+        ++numUsers;
+        addedUser = true;
+        socket.emit('login', {
+            numUsers: numUsers
+        });
+        socket.broadcast.emit('user joined', {
+            id: socket.id,
+            numUsers: numUsers
+        });
+    });
+    
+    socket.on("disconnect", () => {
+        if (addedUser){ 
+            --numUsers;
+            socket.broadcast.emit("user left", {
+                id: socket.id,
+                numUsers: numUsers
+            });
+        }
     });
 });
 
-const PORT = process.env.POST || 3001;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
-    console.log("Server started on port 3001");
-})
+    console.log(`Server started on port ${PORT}`);
+});
--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -1,28 +1,30 @@
 'use client'
 
 import React, { useEffect, useState} from 'react';
-import io from 'socket.io-client'
+import io from 'socket.io-client';
 
 export default function ChatComponent() {
+    const [message, setMessage] = useState('');
     const [messages, setMessages] = useState<string[]>([]);
-
+    
+    // const socket = io(`${process.env.BASE_URL}`);
+    const socket = io("http://localhost:3001");
     useEffect(() => {
-        const socket = io("http://locahost:3001");
-
         socket.on('connect', () => {
             console.log("Connected to the server");
-        })
+        });
         socket.on('disconnect', () => {
             console.log("Disconnected from server")
-        })
+        });
         socket.on("message", (message: string) => {
             console.log("New message received: ", message);
             setMessages((prevMessages) => [...prevMessages, message]);
-        })
-        return () => {
-            socket.disconnect()
-        }
+        });
     }, [])
+    
+    const sendMessage = () => {
+        socket.emit("message", message)
+    }
 
     return (
         <div>
@@ -32,6 +34,17 @@ export default function ChatComponent() {
                     <li key={index}>{message}</li>
                 ))}
             </ul>
+            <div className='flex gap-3'>
+                <input 
+                    className='appearance-none bg-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none' 
+                    value={message} 
+                    onChange={(e) => setMessage(e.target.value)}
+                    placeholder='Write your message here!'
+                />
+                <button 
+                    className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded' 
+                    onClick={sendMessage}>Send</button>
+            </div>
         </div>
     );
 }
